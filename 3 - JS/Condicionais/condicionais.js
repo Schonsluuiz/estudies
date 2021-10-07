6032b925c19c7eace0919c78184efa32468cb3d1
Luiz Schons <schonsluuiz@gmail.com>
2021-10-07 08:26:52 -0300

    if else

--- 3 - JS/Condicionais/condicionais.js
+++ 3 - JS/Condicionais/condicionais.js
@@ -1,5 +1,11 @@
-let vel = 100
+let vel = 110
 
-if (vel <= 100) {
-    console.log('Estamos em uma velocidade aceitavel')
+if (vel < 40) {
+    console.log('Velocidade baixa')
+} else if (vel <= 100) {
+    console.log('Cuidado! Entrando em velociade de risco')
+} else {
+    console.log('Velocidade de Risco')
 }
+
+(vel > 100) ? console.log('Velocidade mair que 100'): console.log('Velocidade menor que 100')
