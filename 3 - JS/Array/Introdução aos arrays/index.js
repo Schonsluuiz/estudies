1a2d3aa027fcb2928e5fa190b9960a2261f31f47
schons <schonsluuiz@gmail.com>
2021-10-25 22:05:04 -0300

    arrays-studies

---
+++ 3 - JS/Array/Introdução aos arrays/index.js
@@ -0,0 +1,15 @@
+let carrosWolks = ['Gol', 'Golf', 'Polo'];
+console.log(carrosWolks[0]);
+
+let golInfo = ['Gol G5 1.6 2010', 5, true];
+console.log(golInfo[1]);
+
+let carrosFord = new Array('Fusion', 'Ranger', 'Ka');
+console.log(carrosFord[1]);
+
+let getNumber = new Array(5);
+console.log(getNumber);
+//imprime uma array de 5 casas undefined
+
+//usar o [] para criar arrays ja preenchidos
+//new array somente para criar arrays vazios
