43236565ad9ddfcd008fa99713341be7e528bbf4
schons <schonsluuiz@gmail.com>
2021-11-01 21:49:09 -0300

    Javascript + Lógica de programação

---
+++ 3 - JS/Aninhando Objetos e Arrays/index.js
@@ -0,0 +1,52 @@
+let nave = {
+    nome : "SuperNova",
+    tipo: "Batalha",
+    passageiros : ["Luiz", "Clovis", "Michely"]
+}
+
+nave.passageiros.push("Arthur");
+
+//console.log(nave)
+
+
+let carros = [
+    { nome : "Gol",
+    cor: "Branco",
+    acessorios : ["Som Automotivo", " Vidro G5", " Rodas Esportivas"]},
+    
+    { nome : "Polo",
+    cor: "Preto",
+    acessorios : ["Multimidia", " Mola Esportiva", " Pneu perfil baixo"]},
+    
+    { nome : "Civic",
+    cor: "Prata",
+    acessorios : ["Camera de Re", " Banco de Couro", " AirBag"]},
+    
+    { nome : "Golf",
+    cor: "Azul",
+    acessorios : ["Teto Solar", " Multimidia", " Suspensao a Ar"]},
+    
+    { nome : "Triton",
+    cor: "Branco",
+    acessorios : ["Multimidia", " Camera de Re", " 4x4"]}
+    
+]
+
+
+//carros.forEach(carros => {
+    //console.log(carros.nome + " da cor " + carros.cor + " tem " + carros.acessorios)
+    
+//});
+
+
+
+let times = {
+    nome : "Inter",
+    titulos: 50,
+    rival: {
+        nome: "Gremio",
+        titulos: 2,
+    }
+}
+
+//console.log(times.rival.nome)
---
+++ 3 - JS/Comparando objetos e arrays/index.js
@@ -0,0 +1,9 @@
+let estacaoEspecial = {
+    nome : "Receptora",
+    plataformas: 15,
+    modelo: "esferica",
+}
+console.log(estacaoEspecial.nome.toUpperCase()); 
+
+let naves = ["Fenix", "Puller", "Golias"];
+console.log(naves[0].toUpperCase());
