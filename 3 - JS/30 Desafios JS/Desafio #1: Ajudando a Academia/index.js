e078dc7a581a000f08a1b3b8854821090a7fdcb2
Luiz Schons <schonsluuiz@gmail.com>
2021-12-24 12:09:05 -0300

    Desafio 01

--- 3 - JS/30 Desafios JS/Desafio #1: Ajudando a Academia/index.js
+++ 3 - JS/30 Desafios JS/Desafio #1: Ajudando a Academia/index.js
@@ -10,5 +10,11 @@ function mediaAlunos(notas) {
 }
 
 let sala1 = [25, 75]
+let sala2 = [10, 9, 6, 8, 9, 1, 5, 7]
+let sala3 = [2, 5, 7, 1, -2]
+let sala4 = [10, 10, 10, 10, 9]
 
 console.log(mediaAlunos(sala1))
+console.log(mediaAlunos(sala2))
+console.log(mediaAlunos(sala3))
+console.log(mediaAlunos(sala4))
