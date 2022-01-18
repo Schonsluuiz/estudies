aa02a4f3616690481a1eead5982c68e59598616f
schons <82298137+sschonss@users.noreply.github.com>
2022-01-18 08:26:56 -0400

    Update index.js

--- 3 - JS/30 Desafios JS/Desafio #4: Cálculos de Viagens Espaciais I/index.js
+++ 3 - JS/30 Desafios JS/Desafio #4: Cálculos de Viagens Espaciais I/index.js
@@ -1,9 +1,7 @@
-//Escreva uma funcao que recebe um numero inteiro qualquer, eleve ao quadrado cada um de seus algarismos e despois concatene o resultado retornando um unico numero inteiro
-
 let num = 3514;
 
 function value(num) {
-  let numString = num + "";
+  let numString = num.toString();
   let uni = numString.split("");
   let res = "";
   for (i = 0; i < numString.length; i++) {
